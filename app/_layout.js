d71218c40712cc6cf2fbf0bdf45abdcec48f48fe
Nicholas K. Hu <94340638+NichKHu@users.noreply.github.com>
2023-04-06 17:26:58 -1200

    Worked on header buttons and welcome component

--- app/_layout.js
+++ app/_layout.js
@@ -1,4 +1,9 @@
 import { Stack } from 'expo-router';
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync(); // When app is loading, it will make splash screen still visible
 
 const Layout = () => {
     return <Stack />
