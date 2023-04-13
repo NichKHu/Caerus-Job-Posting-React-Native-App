61c63857382c5ef0f51dd8035360365bcfe4b85e
Nicholas K. Hu <94340638+NichKHu@users.noreply.github.com>
2023-04-12 20:02:12 -1200

    Set up API key via env and created options variable for fetching at search endpoint

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,10 +1,24 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import { RAPID_API_KEY } from '@env';
 
-const useFetch = () => {
+const rapidApiKey = RAPID_API_KEY; // Initializing earlier for better loading performance
+
+const useFetch = (endpoint, query) => {
 
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
 
+    const axios = require("axios");
+
+    const options = {
+    method: 'GET',
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+        'X-RapidAPI-Key': rapidApiKey,
+        'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+    },
+    params: { ...query },
+    };
 }
