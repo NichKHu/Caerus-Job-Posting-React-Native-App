4cb0cff6ebaf4b88d08dc2d0fb939a3720534752
Nicholas K. Hu <94340638+NichKHu@users.noreply.github.com>
2023-04-12 16:27:12 -1200

    Intialized fonts in layouts

--- app/_layout.js
+++ app/_layout.js
@@ -6,7 +6,20 @@ import * as SplashScreen from 'expo-splash-screen';
 SplashScreen.preventAutoHideAsync(); // When app is loading, it will make splash screen still visible
 
 const Layout = () => {
-    return <Stack />
-}
+    const [fontsLoaded] = useFonts({
+        DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+        DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+        DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+    });
 
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded) {
+            SplashScreen.hideAsync(); // When fonts are loaded, it will hide the splash screen 
+        }
+    }, [fontsLoaded]); 
+
+    if (!fontsLoaded) return null;
+
+    return <Stack onLayout={onLayoutRootView} />
+}
 export default Layout;
